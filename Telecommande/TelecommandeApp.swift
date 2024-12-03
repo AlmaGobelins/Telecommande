7c3c32feea768ad9cf29e0447fe3eb6951052822
Killian El Attar <119617920+Tatar73@users.noreply.github.com>
2024-12-03 15:45:42 +0100

    Initial Commit

---
+++ Telecommande/TelecommandeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelecommandeApp.swift
+//  Telecommande
+//
+//  Created by Killian El Attar on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TelecommandeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
